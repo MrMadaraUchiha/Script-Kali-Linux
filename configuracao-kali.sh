e78699e1aeca94f4b6e1e2bac81466268300adc2
Thales Maggot <122075677+MrMadaraUchiha@users.noreply.github.com>
2023-03-04 17:32:21 -0300

    Update configuracao-kali.sh

--- configuracao-kali.sh
+++ configuracao-kali.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-# Script para configuração inicial do Kali Linux para testes de segurança
+Script para configuração inicial do Kali Linux para testes de segurança
+set -e # Abortar o script caso algum comando falhe
 
-# Atualizar repositórios
-apt update
+Atualizar repositórios
+sudo apt update -y
 
-# Instalar ferramentas básicas de segurança
-apt install -y nmap nikto burp
+Instalar ferramentas básicas de segurança
+sudo apt install -y nmap nikto burp
 
-# Instalar pacotes para ataque de senha
-apt install -y john hydra
+Instalar pacotes para ataque de senha
+sudo apt install -y john hydra
 
-# Instalar pacotes para exploração de vulnerabilidades
-apt install -y metasploit-framework exploitdb
+Instalar pacotes para exploração de vulnerabilidades
+sudo apt install -y metasploit-framework exploitdb
 
-# Instalar pacotes para análise de tráfego de rede
-apt install -y tcpdump wireshark tshark
+Instalar pacotes para análise de tráfego de rede
+sudo apt install -y tcpdump wireshark tshark
 
-# Instalar pacotes para análise de malware
-apt install -y clamav volatility
+Instalar pacotes para análise de malware
+sudo apt install -y clamav volatility
 
-# Configurar usuário root para acesso remoto
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-systemctl restart sshd
+Configurar usuário root para acesso remoto
+sudo sed -i 's/.PermitRootLogin./PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
 
-# Configurar firewall
-ufw enable
-ufw allow ssh
-ufw allow http
-ufw allow https
+Configurar firewall
+sudo ufw enable
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
 
-# Finalização da instalação
+Finalização da instalação
 echo "Configuração inicial concluída com sucesso!"
